f2baa33b2f95b9b491e3af476c01fa346de8bf16
madonuko <madonuko@outlook.com>
2026-08-12 11:31:57 +0800

    fix: change layouts maybe

--- generate-book.py
+++ generate-book.py
@@ -3,8 +3,6 @@
 """
 This auto-generates the mdBook SUMMARY.md file based on the layout on the filesystem.
 
-This generates the `src` directory based on the contents of the `text` directory.
-
 Most RFCs should be kept to a single chapter. However, in some rare cases it
 may be necessary to spread across multiple pages. In that case, place them in
 a subdirectory with the same name as the RFC. For example:
@@ -25,17 +23,7 @@ import shutil
 import subprocess
 
 def main():
-    if os.path.exists('src'):
-        # Clear out src to remove stale links in case you switch branches.
-        shutil.rmtree('src')
-    os.mkdir('src')
-
-    for path in os.listdir('text'):
-        symlink(f'../text/{path}', f'src/{path}')
-    # symlink(f'../compiler_changes.md', f'src/compiler_changes.md')
-    # symlink(f'../lang_changes.md', f'src/lang_changes.md')
-    # symlink(f'../libs_changes.md', f'src/libs_changes.md')
-    symlink('../README.md', 'src/introduction.md')
+    symlink('./README.md', 'src/introduction.md')
 
     with open('src/SUMMARY.md', 'w') as summary:
         summary.write('[Introduction](introduction.md)\n\n')
@@ -44,8 +32,7 @@ def main():
         # summary.write('- [Guidelines for library changes](libs_changes.md)\n')
         collect(summary, 'text', 0)
 
-    symlink('../book.toml', 'src/book.toml')
-    subprocess.call(['mdbook', 'build', '-d', '../book'], cwd='src')
+    subprocess.call(['mdbook', 'build'])
 
 def collect(summary, path, depth):
     entries = [e for e in os.scandir(path) if e.name.endswith('.md')]
